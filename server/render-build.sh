14adb44762d42a190356d4912a64304de0139919
tabish-core <tabish.core@gmail.com>
2026-05-12 12:57:38 +0500

    Update render-build.sh

--- server/render-build.sh
+++ server/render-build.sh
@@ -2,15 +2,8 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
 npm install
 
-# Install Chrome for Puppeteer on Render
-# Note: This is a common way to ensure Puppeteer finds a browser on Render's Linux environment
-if [[ ! -z "$RENDER_INSTANCE_ID" ]]; then
-  echo ">>> Install Chrome dependencies for Puppeteer..."
-  # Render's standard environment already has some libs, but for reliability:
-  # This section is usually handled by the environment variables we set in the dashboard
-  # However, we can also trigger a browser download here if needed.
-  # For now, we rely on the PUPPETEER_EXECUTABLE_PATH pointing to Render's pre-installed Chrome.
-fi
+# In Puppeteer 20+, we use this command to install the browser in the build environment
+echo ">>> Installing Puppeteer Browser..."
+npx puppeteer browsers install chrome
