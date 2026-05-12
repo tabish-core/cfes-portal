71d500e74331558ed02b129114e5e5a66d907544
tabish-core <tabish.core@gmail.com>
2026-05-12 12:57:36 +0500

    Update generateCCRPDF.js

--- server/services/documentService/generateCCRPDF.js
+++ server/services/documentService/generateCCRPDF.js
@@ -62,7 +62,6 @@ const generateCCRPDF = async (data) => {
 
     browser = await puppeteer.launch({
       headless: "new",
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     
